68f04f515a9ba96b465470b202eb66947ddbc0a7
Nezeeeeee <serezha_knyazev_92@mail.ru>
2019-02-28 20:04:48 +0300

    Добавьте файлы проекта.

---
+++ CSharpTroelsen/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTroelsen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+            string input = Console.ReadLine();
+            if (input == "123")
+            {
+                Console.WriteLine("Пароль верен!");
+
+            }
+            else if (input == "321")
+            {
+                Console.WriteLine("Наоборот!");
+            }
+            else
+            {
+                Console.WriteLine("Пароль не верен!");
+            }
+
+
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine(Convert.ToInt32(num1) + Convert.ToInt32(num2));
+        }
+        
+    }
+
+
+   
+}
